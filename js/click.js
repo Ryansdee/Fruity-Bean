babb3d57481e5d70a5e30a6da7e62e6f58d974a3
64576 <64576@etu.he2b.be>
2025-06-03 13:42:31 +0200

    modified:   js/click.js

--- js/click.js
+++ js/click.js
@@ -2,4 +2,4 @@ function redirectRed() {
     window.location.href = "fruits-rouges.html";
 }
 
-document.getElementById("version").innerHTML = "V 0.1.42";
+document.getElementById("version").innerHTML = "V 0.1.43";
